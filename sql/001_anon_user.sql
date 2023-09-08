4f9b4f4725def9121a783541aa41a5ded6d20dc0
Luiz Oliveira <luizsoliveira@gmail.com>
2023-09-07 20:49:15 -0700

    Removing the creation of public schema

--- sql/001_anon_user.sql
+++ sql/001_anon_user.sql
@@ -4,9 +4,6 @@ CREATE ROLE api_anon_user nologin;
 -- Allow app_user switch to api_anon_user
 GRANT api_anon_user TO app_user;
 
--- Create schema public if not exists
-CREATE SCHEMA IF NOT EXISTS public;
-
 -- Allow api_anon_user to use the public schema
 GRANT usage on schema public to api_anon_user;
 
