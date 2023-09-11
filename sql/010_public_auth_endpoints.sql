eaf1a9992181fd5bb70a940b2bf7742d61aa49ec
Luiz Oliveira <luizsoliveira@gmail.com>
2023-09-10 20:01:35 -0700

    Adding a authentication layer to the template api

---
+++ sql/010_public_auth_endpoints.sql
@@ -0,0 +1,39 @@
+-- SQL statements based on PostgREST documentation
+-- aivailable at: https://postgrest.org/en/stable/how-tos/sql-user-management.html#sql-user-management
+
+-- JWT from SQL
+
+ALTER DATABASE app_db SET "app.jwt_secret" TO 'reallyreallyreallyreallyverysafe';
+
+-- add type
+CREATE TYPE basic_auth.jwt_token AS (
+  token text
+);
+
+-- login should be on your exposed schema
+create or replace function
+login(email text, pass text) returns basic_auth.jwt_token as $$
+declare
+  _role name;
+  result basic_auth.jwt_token;
+begin
+  -- check email and password
+  select basic_auth.user_role(email, pass) into _role;
+  if _role is null then
+    raise invalid_password using message = 'invalid user or password';
+  end if;
+
+  select jwt.sign(
+      row_to_json(r), current_setting('app.jwt_secret')
+    ) as token
+    from (
+      select _role as role, login.email as email,
+         extract(epoch from now())::integer + 60*60 as exp
+    ) r
+    into result;
+  return result;
+end;
+$$ language plpgsql security definer;
+
+grant execute on function login(text,text) to api_anon_user;
+
